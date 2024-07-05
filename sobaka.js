59856cfde12840a47400b997fc76f0e751563e37
Kirill Shkurkovsky <beezeshow1674@gmail.com>
2024-07-05 15:09:24 +0300

    Создан репозиторий с проектом

---
+++ sobaka.js
@@ -0,0 +1,16 @@
+
+    const arr = [1,2,3,4,5,6,7,8,9]
+
+    function 4etnie (){
+
+        let result = []
+
+        for (let i = 0; i <= arr.length-1; i++ ){
+
+         if (arr[i]%2 == 0){
+            result.push(arr[i])
+        }
+
+}
+return result
+}
